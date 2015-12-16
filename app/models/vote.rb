985d3962d0b2aab7d33fd4890200d39f5c8bc1a2
Guillermo Rodriguez Cano <wschutz@gmail.com>
2015-12-16 01:32:13 +0100

    [FEATURE] Updated Vote model to include some more relations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,9 @@
 class Vote < ActiveRecord::Base
   self.primary_keys = :voter_id, :votable_id, :votable_type
 
-  belongs_to  :voter,     :foreign_key => :voter_id
+  belongs_to  :voter,     :foreign_key => :voter_id, :class_name => Author
+
   belongs_to  :votable,   :polymorphic => true
+  belongs_to  :comment,   -> { where(:votes => {:votable_type => 'Comment'}) },   :foreign_key => :votable_id
+  belongs_to  :news,      -> { where(:votes => {:votable_type => 'News'}) },      :foreign_key => :votable_id
 end
